6a079c303008ae3865e41106cc26cadb3a9ffe73
zhangxinlin <zhangxinlin@meituan.com>
2018-02-14 15:37:18 +0800

    update

---
+++ src/plugin/webpack.config-origin.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader'],
+                }),
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: `[name]_[contenthash:8].css`,
+        }),
+    ]
+};
+
--- src/plugin/webpack.config.js
+++ src/plugin/webpack.config.js
@@ -1,26 +1,40 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: './index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: ['css-loader'],
-                }),
-            }
-        ]
-    },
-    plugins: [
+module.exports = function (env = {}, argv) {
+    const plugins = [
         new ExtractTextPlugin({
             filename: `[name]_[contenthash:8].css`,
         }),
-    ]
-};
+    ];
 
+    const isProduction = env['production'];
+
+    // 在生成环境才压缩
+    if (isProduction) {
+        plugins.push(
+            new UglifyJsPlugin()
+        )
+    }
+
+    return {
+        entry: './index.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, './dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        use: ['css-loader'],
+                    }),
+                }
+            ]
+        },
+        plugins: plugins,
+        devtool: isProduction ? undefined : 'source-map',
+    };
+}
